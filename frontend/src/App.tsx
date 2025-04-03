f282a2f3cf6ccc3a96f1e7f335eb993d6686129a
Clayton Muhoza <muhozaclayton@gmail.com>
2025-04-03 14:51:49 +0200

    add keycloak to the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
 
 import './App.css'
+import useKeycloak from './hook/useKeyCloak'
 
 function App() {
-
+  const {authenticated,keycloak} = useKeycloak()
   return (
     <>
       
+      <div>
+      <h1>Welcome to the Home Page!</h1>
+      {authenticated ? (
+        <div>
+          <p>Hello, {keycloak?.idTokenParsed?.preferred_username}!</p>
+        </div>
+      ) : (
+        <div>
+          <p>Please log in to access your personalized content.</p>
+          <button onClick={() => keycloak?.login()}>Login</button>
+        </div>
+      )}
+    </div>
     </>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { KeycloakProvider } from './context/KeycloackContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <KeycloakProvider>
+      <App />
+    </KeycloakProvider>
+    
   </StrictMode>,
 )
